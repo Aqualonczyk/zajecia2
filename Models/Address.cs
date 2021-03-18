7c3a508e6ef263e9db94d4417fe9f41de61a275a
Gerald Aqualonczyk <Aqualonczyk@wp.pl>
2021-03-18 12:17:55 +0100

    Add project files.

---
+++ Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace zajecia2.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica")]
+        [StringLength(60, MinimumLength =3), Required(ErrorMessage ="Pole jest obowiązkowe")]
+        public string Street { get; set; }
+        [Display(Name = "ZipCode")]
+        public string ZipCode { get; set; }
+        [Display(Name = "City")]
+        public string City { get; set; }
+        [Display(Name = "Number")]
+        public string Number { get; set; }
+    }
+}
